a590ac68c106f9fb557cdd0f19e58ccae26258c8
victorAponteParedes <vaponte520@gmail.com>
2023-09-19 23:17:01 -0400

    Agregando los modelos en el admin para poder mostrarse

--- store_products/admin.py
+++ store_products/admin.py
@@ -1,5 +1,9 @@
+#IMPORTANDO MODULOS DE DJANGO
 from django.contrib import admin
-from .models import Usuario
+from django.utils.html import format_html
+
+#IMPORTANDO MODULOS INTERNOS
+from .models import Usuario, Producto, Categoria
 
 # Register your models here.
 
@@ -7,3 +11,22 @@ class UsuarioAdmin(admin.ModelAdmin):
     list_display = ["username", "aprobado"]
 
 admin.site.register(Usuario, UsuarioAdmin)
+
+
+
+admin.site.register(Categoria)
+
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "estado", "mostrar_imagen"]
+
+    def mostrar_imagen(self, obj):
+        if obj.imagen:
+            return format_html('<img src="{}" style="border-radius: 50%; width: 40px; height: 40px;" />', obj.imagen.url)
+        else:
+            return "Sin imagen"
+
+    mostrar_imagen.short_description = "Imagen"
+
+admin.site.register(Producto, ProductoAdmin)
