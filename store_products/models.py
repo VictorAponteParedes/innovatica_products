0a0d16f69bb5d11f3f95a1872fd5ffa97d25ddb4
victorAponteParedes <vaponte520@gmail.com>
2023-09-19 21:07:05 -0400

    Serializando, creando un Viewset, y creando el modelo usuario

--- store_products/models.py
+++ store_products/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+class Usuario(AbstractUser):
+    APROBADO_CHOICES = (
+        ('aprobado', 'Aprobado'),
+        ('desaprobado', 'Desaprobado'),
+    )
+
+    aprobado = models.CharField(max_length=12, choices=APROBADO_CHOICES, default='desaprobado')
--- store_products/views.py
+++ store_products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+#IMPORTANDO MODULOS DE REST FRAMEWORK
+from rest_framework import viewsets
+
+#IMPORTANDO MODULOS INTERNOS
+from .serializers import UsuarioSerializer
+from .models import Usuario
 
 # Create your views here.
+
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    serializer_class = UsuarioSerializer
+    queryset = Usuario.objects.all()
