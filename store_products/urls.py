4a644814dcaa2100c3bd2249a07e5cba0a5fe901
victorAponteParedes <vaponte520@gmail.com>
2023-09-19 23:22:29 -0400

    Configurando las rutas

--- store_products/urls.py
+++ store_products/urls.py
@@ -1,19 +1,28 @@
-#IMPORTANDO MODULOS DE REST FRAMEWORK
-from rest_framework.routers import DefaultRouter
-
-#IMPORTANDO MODULOS DE DJANGO
+#IMPORTANDO LOS MODULOS INTERNOS
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import (
+    UsuarioViewSet,
+    CategoriaViewSet,
+    ProductoViewSet,
+    registrar_usuario,
+)
 from django.urls import path, include
 
-#IMPORTANDO MODULOS INTERNOS
-from .views import UsuarioViewSet
-
+#IMPORTANDO LOS MODULOS REST FRAMEWORK
+from rest_framework.routers import DefaultRouter
 
 routers = DefaultRouter()
 routers.register(r"usuarios", UsuarioViewSet, basename="usuarios")
+routers.register(r"categorias", CategoriaViewSet, basename="categorias")
+routers.register(r"productos", ProductoViewSet, basename="productos")
 
 urlpatterns = [
-    path("", include(routers.urls))
+    path("", include(routers.urls)),
+    path("registrar_usuario/", registrar_usuario, name="registrar_usuario"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
